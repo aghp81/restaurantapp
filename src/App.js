48acc2640068c72eeba3414855899a857a1cb94d
abol ghana <aghp81@gmail.com>
2022-09-24 20:49:20 +0330

    Header.jsx - avatar iamge

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Header } from "./components";
 
 const App = () => {
     return (
-        <div className="w-screen h-auto flex flex-col">
+        <div className="w-screen h-auto flex flex-col bg-primary">
             
             <Header />
+
+            <main className="mt-24 p-8 w-full">Maincontainer</main>
         </div>
     )
 }
