f541b0cfd7e60681a8c7b4c0831af527683dd8f5
abol ghana <aghp81@gmail.com>
2022-09-25 10:40:55 +0330

    .env firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import { CreateContainer, Header, MainContainer } from "./components";
 
 const App = () => {
     return (
-        <div className="w-screen h-auto flex flex-col bg-primary">
+        <AnimatePresence>
+            <div className="w-screen h-auto flex flex-col bg-primary">
             
-            <Header />
+                <Header />
 
-            <main className="mt-24 p-8 w-full">
-                <Routes>
-                    <Route path="/*" element={<MainContainer />} />
-                    <Route path="/createItem" element={<CreateContainer />} />
-                </Routes>
-            </main>
-        </div>
+                <main className="mt-24 p-8 w-full">
+                    <Routes>
+                        <Route path="/*" element={<MainContainer />} />
+                        <Route path="/createItem" element={<CreateContainer />} />
+                    </Routes>
+                </main>
+            </div>
+        </AnimatePresence>
+        
     )
 }
 
