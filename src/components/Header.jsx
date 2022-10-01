5c05313db6fa4122f259d7e3b39d4b2341f0010d
abol ghana <aghp81@gmail.com>
2022-10-01 20:44:03 +0330

    Header isMenu setState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { MdShoppingBasket } from "react-icons/md";
+import React, { useState } from "react";
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from "framer-motion";
 
 //google firebase Authentication 
@@ -10,6 +10,8 @@ import { app } from "../firebase.gonfig";
 import Logo from "./../img/logo.png";
 import Avatar from "./../img/avatar-2.png";
 import { Link } from "react-router-dom";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
 
 const Header = () => {
@@ -17,9 +19,20 @@ const Header = () => {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
+
+    const [{user}, dispatch] =useStateValue();
+
+    const [isMenu, setIsMenu] = useState(false);
+
     const login = async () => {
-        const response = await  signInWithPopup(firebaseAuth, provider);
-        console.log(response);
+        if (!user) {
+            const {user: {refreshToken, providerData}} = await  signInWithPopup(firebaseAuth, provider);
+            dispatch({
+                type: actionType.SET_USER,
+                user: providerData[0],
+            }); 
+            localStorage.setItem('user', JSON.stringify(providerData[0])); 
+        }
     };
 
     return (
@@ -58,11 +71,34 @@ const Header = () => {
                 <div className="relative">
                     <motion.img 
                     whileTap={{scale: 0.6}}
-                    src={Avatar} 
-                    className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
+                    src={user ? user.photoURL : Avatar} 
+                    className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
                     alt="userprofile" 
                     onClick={login}
                     />
+                    {
+                        isMenu && (
+                            <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                        {
+                            user && user.email === "aghp81@gmail.com" && (
+                                <Link to={"/createItem"}>
+                                    <p className="px-4 py-2 flex items-center gap-3
+                                    cursor-pointer hover:bg-slate-100 transition-all
+                                    duration-100 ease-in-out text-textColor
+                                     text-base">New Item <MdAdd />
+                                    </p>
+                                </Link>
+
+                            )
+                        }
+                        <p className="px-4 py-2 flex items-center gap-3
+                         cursor-pointer hover:bg-slate-100 transition-all
+                          duration-100 ease-in-out text-textColor text-base">
+                            Logout <MdLogout />
+                        </p>
+                    </div>
+                        )
+                    }
                 </div>
 
                 </div>
