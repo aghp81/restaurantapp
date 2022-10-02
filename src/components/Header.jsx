f16ebf921588d25c005c41dae6ab37b733f26f73
abol ghana <aghp81@gmail.com>
2022-10-02 08:33:08 +0330

    Header.jsx   {/* mobile */}

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
     };
 
     return (
-        <header className="fixed z-50 w-screen p-6 px-16">
+        <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
             {/* desktop & tablet */}
             <div className="hidden md:flex w-full h-full items-center justify-between">
                 <Link to={"/"} className="flex item-center gap-2">
