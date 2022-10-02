de1a0faf4ad7405e65f34ce1166129747e8723f7
abol ghana <aghp81@gmail.com>
2022-10-02 08:48:34 +0330

    Header.jsx  {/* mobile */} ul

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -147,10 +147,7 @@ const Header = () => {
                                 </Link>
                             )}
 
-            <motion.ul initial={{opacity : 0, x : 200}}
-                animate={{opacity : 1, x : 0}}
-                exit={{opacity : 0, x : 200}}
-                className="flex items-center gap-8">
+            <ul className="flex flex-col px-4 py-2 gap-8">
                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
                         Home
                     </li>
@@ -163,7 +160,7 @@ const Header = () => {
                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
                         Service
                     </li>
-                </motion.ul>
+                </ul>
                             
                         <p className="px-4 py-2 flex items-center gap-3
                          cursor-pointer hover:bg-slate-100 transition-all
