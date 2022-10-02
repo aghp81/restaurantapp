f543c0dcefcfde03c14e1691509f303239bcc7ad
abol ghana <aghp81@gmail.com>
2022-10-02 08:34:59 +0330

    Header.jsx  {/* mobile */} Avatar image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -120,6 +120,44 @@ const Header = () => {
                     <img src={Logo} className="w-8 object-cover" alt="logo" />
                     <p className="text-headingColor text-xl font-bold">City</p>
                 </Link>
+
+                <div className="relative">
+                    <motion.img 
+                    whileTap={{scale: 0.6}}
+                    src={user ? user.photoURL : Avatar} 
+                    className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+                    alt="userprofile" 
+                    onClick={login}
+                    />
+                    {
+                        isMenu && (
+                            <motion.div
+                              initial={{opacity : 0, scale : 0.6}}
+                              animate={{opacity : 1, scale : 1}}
+                              exit={{opacity : 0, scale : 0.6}}
+                              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                        {
+                            user && user.email === "aghp81@gmail.com" && (
+                                <Link to={"/createItem"}>
+                                    <p className="px-4 py-2 flex items-center gap-3
+                                    cursor-pointer hover:bg-slate-100 transition-all
+                                    duration-100 ease-in-out text-textColor
+                                     text-base">New Item <MdAdd />
+                                    </p>
+                                </Link>
+
+                            )
+                        }
+                        <p className="px-4 py-2 flex items-center gap-3
+                         cursor-pointer hover:bg-slate-100 transition-all
+                          duration-100 ease-in-out text-textColor text-base">
+                            Logout <MdLogout />
+                        </p>
+                    </motion.div>
+                        )
+                    }
+                </div>
+                
             </div>
         </header>
     )
