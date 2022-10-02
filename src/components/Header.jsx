e9339b4d6737e0b6a4fe55202b732f304983495f
abol ghana <aghp81@gmail.com>
2022-10-02 08:40:15 +0330

    Header.jsx  {/* mobile */} ul

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -145,9 +145,26 @@ const Header = () => {
                                      text-base">New Item <MdAdd />
                                     </p>
                                 </Link>
+                            )}
 
-                            )
-                        }
+            <motion.ul initial={{opacity : 0, x : 200}}
+                animate={{opacity : 1, x : 0}}
+                exit={{opacity : 0, x : 200}}
+                className="flex items-center gap-8">
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                        Home
+                    </li>
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                        Menu
+                    </li>
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                        About Us
+                    </li>
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                        Service
+                    </li>
+                </motion.ul>
+                            
                         <p className="px-4 py-2 flex items-center gap-3
                          cursor-pointer hover:bg-slate-100 transition-all
                           duration-100 ease-in-out text-textColor text-base">
