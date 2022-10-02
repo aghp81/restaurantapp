a1d1e66872dcdcdd87e882b530a1c5c99756a4ae
abol ghana <aghp81@gmail.com>
2022-10-02 08:26:47 +0330

    Header.jsx ul.motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,9 +19,22 @@ const Header = () => {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
+
+    const [{user}, dispatch] =useStateValue();
+
+    const [isMenu, setIsMenu] = useState(false);
+
     const login = async () => {
-        const response = await  signInWithPopup(firebaseAuth, provider);
-        console.log(response);
+        if (!user) {
+            const {user: {refreshToken, providerData}} = await  signInWithPopup(firebaseAuth, provider);
+            dispatch({
+                type: actionType.SET_USER,
+                user: providerData[0],
+            }); 
+            localStorage.setItem('user', JSON.stringify(providerData[0])); 
+        }else {
+            setIsMenu(!isMenu);
+        }
     };
 
     return (
@@ -34,7 +47,10 @@ const Header = () => {
                 </Link>
 
                 <div className="flex items-center gap-8">
-                <ul className="flex items-center gap-8">
+                <motion.ul initial={{opacity : 0, x : 200}}
+                animate={{opacity : 1, x : 0}}
+                exit={{opacity : 0, x : 200}}
+                className="flex items-center gap-8">
                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
                         Home
                     </li>
@@ -47,7 +63,7 @@ const Header = () => {
                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
                         Service
                     </li>
-                </ul>
+                </motion.ul>
 
                 <div className="relative flex items-center justify-center">
                     <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
