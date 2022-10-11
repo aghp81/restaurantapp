6af68a7226e2aff39b7971d8b9664da58738ded5
abol ghana <aghp81@gmail.com>
2022-10-11 08:53:43 +0330

    HomeContainer.jsx - MainContainer.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,6 +37,16 @@ const Header = () => {
         }
     };
 
+    const logout = () => {
+        setIsMenu(false);
+        localStorage.clear();
+
+        dispatch({
+            type : actionType.SET_USER,
+            user : null
+        });
+    };
+
     return (
         <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
             {/* desktop & tablet */}
@@ -102,7 +112,9 @@ const Header = () => {
                         }
                         <p className="px-4 py-2 flex items-center gap-3
                          cursor-pointer hover:bg-slate-100 transition-all
-                          duration-100 ease-in-out text-textColor text-base">
+                          duration-100 ease-in-out text-textColor text-base"
+                          onClick={logout}
+                          >
                             Logout <MdLogout />
                         </p>
                     </motion.div>
@@ -116,6 +128,15 @@ const Header = () => {
 
             {/* mobile */}
             <div className="flex item-center justify-between md:hidden w-full h-full">
+                
+
+                <div className="relative flex items-center justify-center">
+                    <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
+                    <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                        <p className="text-xs text-white font-semibold">2</p>
+                    </div>
+                </div>
+
                 <Link to={"/"} className="flex item-center gap-2">
                     <img src={Logo} className="w-8 object-cover" alt="logo" />
                     <p className="text-headingColor text-xl font-bold">City</p>
@@ -147,24 +168,26 @@ const Header = () => {
                                 </Link>
                             )}
 
-            <ul className="flex flex-col px-4 py-2 gap-8">
-                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <ul className="flex flex-col">
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
                         Home
                     </li>
-                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
                         Menu
                     </li>
-                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
                         About Us
                     </li>
-                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
                         Service
                     </li>
                 </ul>
                             
-                        <p className="px-4 py-2 flex items-center gap-3
-                         cursor-pointer hover:bg-slate-100 transition-all
-                          duration-100 ease-in-out text-textColor text-base">
+                        <p className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200
+                         gap-3 cursor-pointer hover:bg-gray-300 transition-all
+                          duration-100 ease-in-out text-textColor text-base"
+                            onClick={logout}
+                          >
                             Logout <MdLogout />
                         </p>
                     </motion.div>
---
+++ src/components/HomeContainer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Delivery from "./img/bike-delivery.png";
+import HeroBg from "./img/heroBg-1.png";
+
+const HomeContainer = () => {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
+      <div className='py-2 flex-1 flex flex-col items-start justify-center gap-6'>
+        <div className="flex items-center gap-2 justify-center bg-orange-100
+        px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">Bike Delivery</p>
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden 
+          drop-shadow-x1">
+            <img src={Delivery} className="w-full h-full object-contain" alt="delivery" />
+          </div>
+        </div>
+
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold -tracking-wide text-headingColor">
+          The fastest Delivery in<span className='text-orange-600 text-[3rem] lg:text-[5rem]'>Your City</span>
+        </p>
+
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore atque corporis reprehenderit repudiandae assumenda aliquid ipsa modi quam cum mollitia, optio quidem hic nesciunt necessitatibus ab, similique dolore ad provident.</p>
+
+        <button type='button' className='bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'>Order Now</button>
+
+      </div>
+      <div className='py-2 flex-1 flex items-center'>
+            <img src={HeroBg} className="ml-auto h-650" alt="hero-bg" />
+
+            <div className="w-full h-full absolute flex items-center justify-center"></div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeContainer
