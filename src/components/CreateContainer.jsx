c381ce6c9070ef2924203c1ac9964492f917fc95
abol ghana <aghp81@gmail.com>
2022-10-18 22:27:36 +0330

    Loader.jsx

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 import {MdFastfood} from 'react-icons/md'
+import { categories } from "../utils/data";
 
 
 
@@ -20,7 +21,7 @@ const CreateContainer = () => {
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
       <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col 
-      items-center justify-center">
+      items-center justify-center gap-4">
         {
           fields && (
             <motion.p initial={{opacity : 0}} 
@@ -46,9 +47,36 @@ const CreateContainer = () => {
           value={title} 
           onChange={(e) => setTitle(e.target.value)}
           placeholder='Give me a title ...'
-          className="w-full h-full text-lg bg-transparent font-semibold"
+          className="w-full h-full text-lg bg-transparent
+          font-semibold outline-none border-none
+          placeholder:text-gray-400 text-textColor"
           />
         </div>
+
+        <div className="w-full">
+          <select onChange={(e) => setCategory(e.target.value)} 
+          className="outline-none w-full text-base border-b-2
+          border-gray-200 p-2 rounded-md cursor-pointer"
+          >
+            <option value="other" className="bg-white">
+              select category
+            </option>
+            {categories && categories.map(item => (
+              <option key={item.id} className="text-base border-0
+              outline-none capitalize bg-white text-headingColor"
+               value={item.urlParaName}
+              >
+                {item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="group flex justify-center items-center flex-col
+         border-2 border-dotted border-gray-300 w-full h-225 md:h-420
+          cursor-pointer rounded-lg">
+            
+          </div>
       </div>
     </div>
   )
--- src/utils/data.js
+++ src/utils/data.js
@@ -10,4 +10,43 @@ export const heroData = [
     {id: 2, name: "Strawberries", decp: "Fresh Strawberries", price: "10.25", imageSrc: F1},
     {id: 3, name: "Chicken Kebab", decp: "Mixed Kebab Plate", price: "8.25", imageSrc: C3},
     {id: 4, name: "Fish Kebab", decp: "Mixed Fish Kebab", price: "5.25", imageSrc: Fi1},
-  ]
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      name: "Chicken",
+      urlParaName: "chicken",
+    },
+    {
+      id: 2,
+      name: "Curry",
+      urlParaName: "curry",
+    },
+    {
+      id: 3,
+      name: "Rice",
+      urlParaName: "rice",
+    },
+    {
+      id: 4,
+      name: "Fish",
+      urlParaName: "fish",
+    },
+    {
+      id: 5,
+      name: "Fruits",
+      urlParaName: "fruits",
+    },
+    {
+      id: 6,
+      name: "Icecreams",
+      urlParaName: "icecreams",
+    },
+    {
+      id: 7,
+      name: "Soft Drinks",
+      urlParaName: "drinks",
+    },
+ ];
+
