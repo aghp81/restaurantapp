944c109e31de994ad233923dd63091f349d0f502
abol ghana <aghp81@gmail.com>
2022-10-30 10:31:10 +0330

    firebaseFunctions.js - foodItems

--- src/utils/firebaseFunctions.js
+++ src/utils/firebaseFunctions.js
@@ -4,5 +4,7 @@ import { setDoc } from "firebase/firestore"
 import { firestore } from "../firebase.gonfig"
 
 export const saveItem = async (data) => {
-    await setDoc(doc(firestore, 'foodItems'))
-}
+    await setDoc(
+        doc(firestore, "foodItems", `&{Date.now()}`, data, {merge : true})
+    );
+};
