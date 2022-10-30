06e4234d1128054855dd982c37fe3ae5bfad5be6
abol ghana <aghp81@gmail.com>
2022-10-30 10:26:15 +0330

    firebaseFunctions.js

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,13 @@
   import React, { useState } from "react";
   import { motion } from "framer-motion";
 
-import {MdFastfood} from 'react-icons/md'
-import { categories } from "../utils/data";
+  import {MdFastfood, MdCloudUpload , MdDelete, MdFoodBank, MdAttachMoney} from 'react-icons/md'
+  import { categories } from "../utils/data";
+
+  import Loader from "./Loader";
+import { storage } from "../firebase.gonfig";
+import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { onSnapshot } from "firebase/firestore";
 
 
 
@@ -18,40 +23,123 @@ import { categories } from "../utils/data";
     const [msg, setMsg] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col 
-      items-center justify-center gap-4">
-        {
-          fields && (
-            <motion.p initial={{opacity : 0}} 
-            animate={{opacity : 1}} 
-            exit={{opacity : 0}} 
-            className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
-              alertStatus === "danger" 
-            ? "bg-red-400 text-red-800" 
-            : "bg-emerald-400 text-emerald-800"
-             }`}
-            >
-              {msg}
-            </motion.p>
-          )
-        }
+    const uploadImage = (e) => {
+      setIsLoading(true);
+      const imageFile = e.target.files[0];
+      //console.log(imageFile);
+      const storageRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`)
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
 
-        <div className="w-full py-2 border-b border-gray-300 flex 
-        items-center gap-2">
-          <MdFastfood className="text-xl text-gray-700" />
-          <input 
-          type="text" 
-          required 
-          value={title} 
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder='Give me a title ...'
-          className="w-full h-full text-lg bg-transparent
-          font-semibold outline-none border-none
-          placeholder:text-gray-400 text-textColor"
-          />
-        </div>
+      uploadTask.on("state_changed", (snapshot) => {
+        const uploadProgress = (snapshot.bytesTransferred / snapshot.
+        totalBytes) * 100;
+      }, (error) => {
+        console.log(error);
+        setFields(true);
+        setMsg('Error while uploading : Try Again');
+        setAlertStatus('danger')
+        setTimeout(() => {
+          setFields(false)
+          setIsLoading(false)
+        }, 4000);
+      }, 
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+          setImageAsset(downloadURL);
+          setIsLoading(false)
+          setFields(true);
+          setMsg('Image uploaded successfully');
+          setAlertStatus('success')
+          setTimeout(() => {
+            setFields(false)
+          }, 4000);
+        })
+      })
+    };
+
+    const deleteImage = () => {
+      setIsLoading(true);
+      const deleteRef = ref(storage, imageAsset);
+      deleteObject(deleteRef).then(( ) => {
+        setImageAsset(null)
+        setIsLoading(false)
+        setFields(true);
+          setMsg('Image deleted successfully');
+          setAlertStatus('success');
+          setTimeout(() => {
+            setFields(false);
+          }, 4000);
+      } );
+    };
+
+    const saveDetails = () => {
+      setIsLoading(true);
+      try {
+        if((!title || !calories || !imageAsset || !price || !category)){
+          setFields(true);
+          setMsg("Required fields can't be empty");
+          setAlertStatus('danger')
+          setTimeout(() => {
+            setFields(false)
+            setIsLoading(false)
+          }, 4000);
+        }else{
+           const data = {
+            id : `${Date.now()}`,
+            title : title,
+            imageURL : imageAsset,
+            category : category,
+            calories : calories,
+            qty : 1,
+            price : price
+           }
+        }
+      } catch (error) {
+        console.log(error);
+        setFields(true);
+        setMsg('Error while uploading : Try Again');
+        setAlertStatus('danger')
+        setTimeout(() => {
+          setFields(false)
+          setIsLoading(false)
+        }, 4000);
+      }
+    };
+
+    return (
+      <div className="w-full min-h-screen flex items-center justify-center">
+        <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col 
+        items-center justify-center gap-4">
+          {
+            fields && (
+              <motion.p initial={{opacity : 0}} 
+              animate={{opacity : 1}} 
+              exit={{opacity : 0}} 
+              className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
+                alertStatus === "danger" 
+              ? "bg-red-400 text-red-800" 
+              : "bg-emerald-400 text-emerald-800"
+              }`}
+              >
+                {msg}
+              </motion.p>
+            )
+          }
+
+          <div className="w-full py-2 border-b border-gray-300 flex 
+          items-center gap-2">
+            <MdFastfood className="text-xl text-gray-700" />
+            <input 
+            type="text" 
+            required 
+            value={title} 
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder='Give me a title ...'
+            className="w-full h-full text-lg bg-transparent
+            outline-none border-none
+            placeholder:text-gray-400 text-textColor"
+            />
+          </div>
 
           <div className="w-full">
             <select onChange={(e) => setCategory(e.target.value)} 
---
+++ src/utils/firebaseFunctions.js
@@ -0,0 +1,7 @@
+// saving new Item
+
+import { setDoc } from "firebase/firestore"
+
+export const saveItem = async (data) => {
+    await setDoc(doc)
+}
