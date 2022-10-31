65fe8837683635920e64fa28190a63fec764490a
abol ghana <aghp81@gmail.com>
2022-10-31 12:01:20 +0330

    firebaseFunctions.js - get all food items

--- src/utils/firebaseFunctions.js
+++ src/utils/firebaseFunctions.js
@@ -1,9 +1,18 @@
 // saving new Item
 
-import { doc, setDoc } from "firebase/firestore";
+import { collection, doc, getDocs, orderBy, query, setDoc } from "firebase/firestore";
 import { firestore } from "../firebase.gonfig";
 
 export const saveItem = async (data) => {
     await setDoc(doc(firestore, "foodItems", `&{Date.now()}`), data, {merge : true,
         });
+};
+
+// get all food items 
+export const getAllFoodItems = async () => {
+    const items = await getDocs(
+        query(collection(firestore, "foodItems"), orderBy("id", "desc"))
+    );
+
+    return items.docs.map((doc) => doc.data());
 };
