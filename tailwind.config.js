d4180e98650090b03f7cb4a923ba74054b7ddb0b
abol ghana <aghp81@gmail.com>
2022-11-13 08:38:41 +0330

    RowContainer.jsx & tailwindcss scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,8 +53,11 @@ module.exports = {
       primary: "#f5f3f3",
       cardOverlay: "rgba(256, 256, 256, 0.4)",
       lighttextgray: "9ca0ab",
+      
     },
   },
 },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 }
