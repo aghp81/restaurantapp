d99fd18a9c749e41a60a9317c43d58fe1fbd78ed
abol ghana <aghp81@gmail.com>
2022-11-18 13:32:36 +0330

    MenuContainer.jsx - IoFastFood

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {IoFastFood} from "react-icons/io5";
 
 const MenuContainer = () => {
   return (
@@ -15,10 +16,15 @@ const MenuContainer = () => {
         <div className="w-full flex items-center justify-start 
         lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none">
             <div className="group bg-card w-24 min-w-[94px] h-28 
-            cursor-pointer rounded-lg drop-shadow-lg 
-            flex flex-col gap-3 items-center justify-center 
+            cursor-pointer rounded-lg drop-shadow-xl 
+            flex flex-col gap-3 items-center justify-center hover:bg-red-600
             duration-150 transition-all ease-in-out">
-                
+                <div className="w-10 h-10 rounded-full bg-red-600 
+                group-hover:bg-card flex items-center justify-center">
+                    <IoFastFood 
+                        className="text-card group-hover:text-textColor text-lg"
+                    />
+                </div>
             </div>
         </div>
     </div>
