72a2abd5796b444f0156d7170cddbee2ebec944c
abol ghana <aghp81@gmail.com>
2022-11-18 20:15:33 +0330

    RowContainer.jsx - put img into a div

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -17,7 +17,7 @@ const RowContainer = ({flag, data, scrollValue}) => {
    className={`w-full flex items-center gap-3 my-12 scroll-smooth ${
     flag 
     ? "overflow-x-scroll scrollbar-none" 
-    : "overflow-x-hidden flex-wrap"
+    : "overflow-x-hidden flex-wrap justify-center"
     }`}
     > 
         {data && data.map(item => (
@@ -27,11 +27,14 @@ const RowContainer = ({flag, data, scrollValue}) => {
           rounded-lg p-2 my-12 shadow-md 
           backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-between">
               <div className="w-full flex items-center justify-between">
-                  <motion.img whileHover={{scale : 1.2 }}
-                  src={item?.imageURL} 
-                  alt=""
-                  className="w-40 -mt-8 drop-shadow-2xl" 
+                <motion.div className="w-40 -mt-8 drop-shadow-2xl" whileHover={{scale : 1.2 }}>
+                  <img
+                    src={item?.imageURL} 
+                    alt=""
+                    className="w-full h-full object-contain" 
                   />
+                </motion.div>
+                  
                   <motion.div whileTap={{scale : 0.75}} 
                   className="w-8 h-8 rounded-full bg-red-600 flex items-center
                   justify-center cursor-pointer hover:shadow-md">
