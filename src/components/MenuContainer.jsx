e599319496b7475233a645a7c1d08d1d1d2af27b
abol ghana <aghp81@gmail.com>
2022-11-18 13:46:48 +0330

    MenuContainer.jsx - data from data.js

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {IoFastFood} from "react-icons/io5";
+import {categories} from "../utils/data";
 
 const MenuContainer = () => {
 
@@ -17,18 +18,22 @@ const MenuContainer = () => {
 
         <div className="w-full flex items-center justify-start 
         lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none">
-            <div className="group bg-card w-24 min-w-[94px] h-28 
-            cursor-pointer rounded-lg drop-shadow-xl 
-            flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg
-            duration-150 transition-all ease-in-out">
-                <div className="w-10 h-10 rounded-full bg-cartNumBg 
-                group-hover:bg-card flex items-center justify-center">
-                    <IoFastFood 
-                        className="text-card group-hover:text-textColor text-lg"
-                    />
+            {categories && categories.map(category => (
+                <div key={category.id} className="group bg-card w-24 min-w-[94px] h-28 
+                cursor-pointer rounded-lg drop-shadow-xl 
+                flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg
+                duration-150 transition-all ease-in-out">
+                    <div className="w-10 h-10 rounded-full bg-cartNumBg 
+                    group-hover:bg-card flex items-center justify-center">
+                        <IoFastFood 
+                            className="text-card group-hover:text-textColor text-lg"
+                        />
+                    </div>
+                    <p className="text-sm text-textColor group-hover:text-white">
+                        {category.name}
+                    </p>
                 </div>
-                <p className="text-sm text-textColor group-hover:text-white">Category</p>
-            </div>
+            ))}
         </div>
     </div>
   </section>
