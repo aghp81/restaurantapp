ef8f3cb147dc511c2283471e7606f5d837dda50d
abol ghana <aghp81@gmail.com>
2022-11-21 10:04:40 +0330

    CartContainer.jsx - copy 2 cart Item

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -28,6 +28,7 @@ const CartContainer = () => {
         <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
           <div className="w-full h-340 md:h-42 px-6 py-10 flex 
           flex-col gap-3 overflow-y-scroll scrollbar-none">
+
             {  /*  cart Item  */  }
             <div className="w-full p-1 px-2 rounded-lg 
             bg-cartItem flex items-center gap-2">
@@ -60,6 +61,72 @@ const CartContainer = () => {
                 </motion.div>
               </div>
             </div>
+
+            <div className="w-full p-1 px-2 rounded-lg 
+            bg-cartItem flex items-center gap-2">
+              <img src={IceCream} alt="" className="w-20 h-20 max-w-[60px] 
+              rounded-full object-contain" />
+
+              {  /*  name section  */  }
+              <div className="flex flex-col gap-2">
+                <p className="text-base text-gray-50">
+                  chocolate vanilla
+                </p>
+                <p className="text-sm block text-gray-300 font-semibold">
+                  $8.5
+                </p>
+              </div>
+
+              {  /*  button section  */  }
+              <div className="group flex items-center gap-2 ml-auto cursor-pointer">
+                <motion.div whileTap={{scale : 0.75}}>
+                  <BiMinus className="text-gray-50" />
+                </motion.div>
+
+                <p className="w-5 h-5 rounded-sm bg-cartBg 
+                text-gray-50 flex items-center justify-center">
+                  1
+                </p>
+
+                <motion.div whileTap={{scale : 0.75}}>
+                  <BiPlus className="text-gray-50" />
+                </motion.div>
+              </div>
+            </div>
+
+            <div className="w-full p-1 px-2 rounded-lg 
+            bg-cartItem flex items-center gap-2">
+              <img src={IceCream} alt="" className="w-20 h-20 max-w-[60px] 
+              rounded-full object-contain" />
+
+              {  /*  name section  */  }
+              <div className="flex flex-col gap-2">
+                <p className="text-base text-gray-50">
+                  chocolate vanilla
+                </p>
+                <p className="text-sm block text-gray-300 font-semibold">
+                  $8.5
+                </p>
+              </div>
+
+              {  /*  button section  */  }
+              <div className="group flex items-center gap-2 ml-auto cursor-pointer">
+                <motion.div whileTap={{scale : 0.75}}>
+                  <BiMinus className="text-gray-50" />
+                </motion.div>
+
+                <p className="w-5 h-5 rounded-sm bg-cartBg 
+                text-gray-50 flex items-center justify-center">
+                  1
+                </p>
+
+                <motion.div whileTap={{scale : 0.75}}>
+                  <BiPlus className="text-gray-50" />
+                </motion.div>
+              </div>
+            </div>
+
+            
           </div>
         </div>
 
