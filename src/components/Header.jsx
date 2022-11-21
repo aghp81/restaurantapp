14f31b696b7583b3a075d73d29b55331c9bd8d30
abol ghana <aghp81@gmail.com>
2022-11-21 16:03:49 +0330

    Header.jsx - showCart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
     const provider = new GoogleAuthProvider();
 
 
-    const [{user}, dispatch] =useStateValue();
+    const [{user, cartShow}, dispatch] =useStateValue();
 
     const [isMenu, setIsMenu] = useState(false);
 
@@ -75,7 +75,7 @@ const Header = () => {
                     </li>
                 </motion.ul>
 
-                <div className="relative flex items-center justify-center">
+                <div className="relative flex items-center justify-center" onClick={showCart}>
                     <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
                     <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
                         <p className="text-xs text-white font-semibold">2</p>
